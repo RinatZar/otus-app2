156a5f1ad4b6bde13a2cd500a3ffe87dd3471800
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-03 13:13:16 +0300

    Update apt_install.sh

--- apt_install.sh
+++ apt_install.sh
@@ -7,6 +7,7 @@ echo "prometheus установлен"
 
 # Установка Grafana
 #sudo apt install adduser libfontconfigi musl -y
+#systemctl enable grafana-server
 systemctl daemon-reload
 systemctl restart grafana-server
 #echo "Grafana установлен"
