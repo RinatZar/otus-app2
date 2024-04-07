389ac232e023b4a93666e4b9650146335f080aae
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-07 11:01:33 +0300

    Update grafana_script.sh

--- grafana_script.sh
+++ grafana_script.sh
@@ -3,16 +3,26 @@
 DIR='/etc/grafana/'
 DIR1='/home/otus-app1/grafana.ini'
 DIR3='/home/otus-app1/provisioning'
+DIR5='/home/otus-app1/prometheus.yml'
 DIR2='/etc/grafana/'
+DIR4='/etc/prometheus/'
 
 #Копируем конфиг 
 cp $DIR1 $DIR2
 cp -r $DIR3 $DIR2
-echo "файл конфиги скопированы в $DIR"
+cp $DIR5 $DIR4
+echo "файл конфиги скопированы"
 systemctl restart grafana-server
 
 if [ $? -eq 0 ]; then
           echo "Granana запущен, готов к работе"
+else
+          echo "Возникла ошибка"
+fi
+
+systemctl restart prometheus
+if [ $? -eq 0 ]; then
+          echo "prometheus запущен, готов к работе"
 else
           echo "Возникла ошибка"
 fi
