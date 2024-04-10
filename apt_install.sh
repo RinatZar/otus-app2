0616bfb6a6b56c618301c176c9ba9b04f864ceaf
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-10 17:02:14 +0300

    Update apt_install.sh

--- apt_install.sh
+++ apt_install.sh
@@ -6,8 +6,11 @@ sudo apt install prometheus -y
 echo "prometheus установлен"
 
 # Установка Grafana
-#sudo apt install adduser libfontconfigi musl -y
-#systemctl enable grafana-server
+dpkg -i /home/otus/grafana_10.2.2_amd64-224190-2cad86.deb
 systemctl daemon-reload
 systemctl restart grafana-server
-#echo "Grafana установлен"
+if [ $? -eq 0 ]; then
+          echo "Grafana запущен, готов к работе"
+else
+          echo "Возникла ошибка, Grafana"
+fi
